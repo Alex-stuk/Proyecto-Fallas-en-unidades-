821d77f12d1d2c086a3cd01d0eaf8e80d56e858c
Alex-stuk <alexstuk17@gmail.com>
2024-08-06 19:08:06 -0600

    Add files via upload

---
+++ FallasEnLaUnidad.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.fallas.en.la.unidad;
+
+import com.mycompany.fallas.en.la.unidad.igu.Principal;
+
+public class FallasEnLaUnidad {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+        
+    }
+}
