100780bb6d242adbe6110538e3f70263fecc7787
Alex-stuk <alexstuk17@gmail.com>
2024-08-06 19:09:05 -0600

    Add files via upload

---
+++ ControladoraPersistencia.java
@@ -0,0 +1,6 @@
+package com.mycompany.fallas.en.la.unidad.Persistencia;
+
+public class ControladoraPersistencia {
+    
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+}
